516c3ad8af2a8852928ab867a0f3c7e4d1ba1153
Ivan Švaljek <ivan.svaljek@gmail.com>
2024-02-21 07:24:06 +0100

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBlazorWebViewClick
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
